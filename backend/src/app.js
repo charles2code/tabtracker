4630e2c4e223573de1f7853e3f454c07eca69345
charles2code <charles2code@gmail.com>
2019-12-15 11:52:18 -0500

    got server up & printed 'SERVER IS UP' on browser port 3000

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,3 +2,23 @@ console.log('------------------------------------');
 console.log("HEllO World!");
 console.log('------------------------------------');
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+app.use(morgan("combine"));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/status', (req, res) => {
+    res.send({
+        message: "Server IS UP!"
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log('App listening on port 3000!');
+});
